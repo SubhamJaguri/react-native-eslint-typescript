93354d8cf02e30f3083bcc3e7c5071e4760c1aed
KishanDoth <kishan@dothdigital.com>
2022-04-02 21:18:27 +0530

    add: gradiant in spash screen

--- src/screens/SplashScreen.tsx
+++ src/screens/SplashScreen.tsx
@@ -1,36 +1,36 @@
 import { StyleSheet} from 'react-native';
 import {
 	Center,
-	NativeBaseProvider,
 	Image
   } from 'native-base';
 import React from 'react';
 import { FOOODIE_NAMEIMAGE, SPLASH_IMAGE } from '../constants/images';
+import LinearGradient from 'react-native-linear-gradient';
 
 const SplashScreen = () => {
 	return (
 		<>
-			<NativeBaseProvider>
-				<Center flex={1} bg="coolGray.700">
-					<Image source={SPLASH_IMAGE} style={styles.logo} />
-					<Image source={FOOODIE_NAMEIMAGE} style={styles.fooodie} />
+			<LinearGradient colors={['#C70923', '#E02D41', '#C70923']} style={{flex:1}}>
+				<Center flex={1}>
+					<Image source={SPLASH_IMAGE} style={styles.logo} alt={'Logo Image'} />
+					<Image source={FOOODIE_NAMEIMAGE} style={styles.logoName} alt={'Logo Name'} />
 				</Center>
-			</NativeBaseProvider>
-		</>
+			</LinearGradient>
+		</>	
 	);
 };
 export default SplashScreen;
 
 const styles = StyleSheet.create({
 	logo: {
-		width: '35%',
-		height: '19%',
+		width: '50%',
+		height: '20%',
 		resizeMode: 'contain',
 	},
-	fooodie: {
+	logoName: {
 		marginTop: 10,
-		width: '52%',
-		height: '9%',
+		width: '55%',
+		height: '10%',
 		resizeMode: 'contain',
 	},
 });
